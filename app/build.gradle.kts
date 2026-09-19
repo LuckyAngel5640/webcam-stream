30a8b6d3e77f0c9af849958ef54c79c2fdc11585
LuckyAngel5640 <alisina8749@gmail.com>
2026-09-19 21:42:08 +0330

    Try WebRTC M113 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.camera:camera-video:1.3.2")
 
     // WebRTC for push streaming - push video to server like CCTV
-    implementation("org.webrtc:google-webrtc:1.0.32006")
+    implementation("org.webrtc:google-webrtc:M113")
 
     // ML Kit - using simple frame differencing for motion detection instead
     // implementation("com.google.mlkit:object-detection:17.2.0")
