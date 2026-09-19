1e5dbb14fff9b8edc2699cd57d516278b46fe0dd
LuckyAngel5640 <alisina8749@gmail.com>
2026-09-19 21:41:17 +0330

    Enable WebRTC for CCTV push streaming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,8 +71,8 @@ dependencies {
     implementation("androidx.camera:camera-view:1.3.2")
     implementation("androidx.camera:camera-video:1.3.2")
 
-    // WebRTC for push streaming - comment out for build, add later with correct version
-    // implementation("org.webrtc:google-webrtc:1.0.32006")
+    // WebRTC for push streaming - push video to server like CCTV
+    implementation("org.webrtc:google-webrtc:1.0.32006")
 
     // ML Kit - using simple frame differencing for motion detection instead
     // implementation("com.google.mlkit:object-detection:17.2.0")
