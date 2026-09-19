3ba2f7caf8c5a17db243f0b4d85205cdfe14772c
LuckyAngel5640 <alisina8749@gmail.com>
2026-09-19 21:29:55 +0330

    Remove app repositories block to use settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,13 +53,7 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
 }
-
-dependencies {
     // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
